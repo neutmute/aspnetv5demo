d3ab829c455a1dff074d58e47046670813e8e403
NM <neutmute@neutmute.nm>
2015-07-07 12:37:26 +1000

    tag helper works

---
+++ src/TagHelperDemo/TagHelpers/ThumbnailTagHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagHelperDemo.TagHelpers
+{
+    /// <summary>
+    /// http://www.codeproject.com/Articles/986917/Introducing-TagHelpers-with-ASP-NET-on-EverLeap
+    /// </summary>
+    [TargetElement("thumbnail", Attributes = "src")]
+    public class ThumbnailTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes["class"] = "thumbnail";
+            var sb = new StringBuilder();
+            sb.AppendFormat("<img src=\"{0}\"", context.AllAttributes["src"].ToString());
+            if (context.AllAttributes.ContainsKey("alt"))
+            {
+                sb.AppendFormat(" alt=\"{0}\"", context.AllAttributes["alt"]);
+            }
+            sb.Append("/>");
+            if (context.AllAttributes.ContainsKey("caption"))
+            {
+                sb.AppendFormat("<div class=\"caption\"><h3>{0}</h3></div>", context.AllAttributes["caption"]);
+            }
+            output.Content.SetContent(sb.ToString());
+
+            base.Process(context, output);
+        }
+    }
+}
